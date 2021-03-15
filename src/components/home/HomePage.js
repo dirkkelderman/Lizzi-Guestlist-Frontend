e47b5e455de3842aad99242294b9258ba76dd1d7
Dirk Kelderman <dirkkelderman@gmail.com>
2021-03-15 13:47:59 +0100

    styled the homepage

--- src/components/home/HomePage.js
+++ src/components/home/HomePage.js
@@ -1,11 +1,35 @@
 import React from 'react'
 import LogoLizzi from './lizzilogo groot geel.png'
+import {Button, Container} from '@material-ui/core';
+import { Link } from "react-router-dom";
+
 
 function HomePage() {
     return (
-        <div >
+        <Container >
             <img style={{width: "100%"}} src={LogoLizzi} alt="Lizzi Logo Groot"></img>
-        </div>
+            
+            <Button
+            type="submit"
+            fullWidth
+            variant="contained"
+            style={{ backgroundColor: "#fad974", color: "black", border: '1px solid black' }}
+            component={Link}
+            to={`/login`}
+          >
+            Log In
+          </Button>
+          <Button
+            type="submit"
+            fullWidth
+            variant="contained"
+            style={{ backgroundColor: "black", color: "white", border: '1px solid #fad974' }}
+            component={Link}
+            to={`/signup`}
+          >
+            Sign Up
+          </Button>
+        </Container>
     )
 }
 
